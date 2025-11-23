e7b6a6b75ceff41157630999a17a7b5f92fb27b6
rishabhpal207-lab <rishabhpal207@gmail.com>
2025-11-23 14:48:36 +0530

    Add basic styles for body, header, and paragraph

---
+++ script.js
@@ -0,0 +1,12 @@
+body {
+    background-color: rgb(187, 243, 238);
+    text-align: center;
+}
+header {
+    text-align: center;
+    background-color: rgb(210, 180, 115);
+}
+
+p{
+    background-color: rgb(187, 243, 238);
+}
